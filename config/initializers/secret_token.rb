47f4083412455f4607a01733567339f87cf5bd2a
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-10-29 16:30:36 -0700

    Initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SEMAT::Application.config.secret_key_base = '5b05dbb43c533247b4760160b56854998be3d2c874a853f28dbcfb6fa765ac7a8439f363f585fdd457d00daac1c3d44eb761baacc08e3a135b4d6282df41b5c0'
