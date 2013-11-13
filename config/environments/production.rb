603042c39e51649c80448edf28662cc0c910594e
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-11-12 16:13:18 -0800

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,13 +80,13 @@ SEMAT::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
+
   config.middleware.use ExceptionNotification::Rack,
     :email => {
       :email_prefix => "[ERROR] ",
       :sender_address => %{"Exception" <support@example.com>},
-      :exception_recipients => %w(todd.sedano@sv.cmu.edu),
-#      :sections => %w{cmusv} + ExceptionNotifier::Notifier.default_sections
+#      :sections => %w{cmusv} + ExceptionNotifier::Notifier.default_sections,
+      :exception_recipients => %w(todd.sedano@sv.cmu.edu)
     }
 
 #  config.middleware.use("Rack::GoogleAnalytics", :web_property_id => "UA-8300440-2")  
