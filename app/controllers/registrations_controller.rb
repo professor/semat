94ec902cf4f89f1234c15d3d185dc74c21ac4eb8
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-12-04 14:13:32 -0800

    Override redirect after signup to alpha page

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def after_sign_up_path_for(resource)
+    alphas_path
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    alphas_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ SEMAT::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
+
   root :to => "welcome#index"
 
   resources :teams
