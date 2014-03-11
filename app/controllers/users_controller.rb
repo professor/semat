69a74c34d5d23afd63516b5c1e1a12e8b2ac108c
Todd Sedano <todd.sedano@sv.cmu.edu>
2014-03-11 12:31:45 -0700

    Default version

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     user = User.where(:email => params[:email]).first
     @teams = user.teams.order(:name)
     @essence_versions = EssenceVersion.all
+    @default_version = "CMU 1.1"
   end
 
 end
